29985d5f0e3fe7e588f4f3526a8a9628aaab650e
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2024-08-01 08:39:06 -0300

    atualizei projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,1 @@
-var swiper = new Swiper(".home-slider",{
-    autoplay:{
-        duration:2500,
-        disableOnInteraction: false,
-    },
-    grapCursor:false,
-    loop:true,
-    centeredSlides:true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-})
+AOS.init();
